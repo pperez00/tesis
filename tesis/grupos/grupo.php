689f8157211cfad7e70a3d64298196b504349af7
Pablo <pablo.s.perez@hotmail.com>
2022-03-07 16:39:31 -0300

    grupos

--- tesis/grupos/grupo.php
+++ tesis/grupos/grupo.php
@@ -1,13 +1,13 @@
 <?php
-class ClaseSencilla
+class Grupo
 {
     // Declaración de una propiedad
     private $imagen_grupo = '../img/mat.png';
     private $nombre_grupo = '';
 
     // Declaración de un método
-    public function mostrarVar() {
-        echo $this->var;
+    public function getNombre() {
+        echo $this->nombre_grupo;
     }
 }
 ?>
--- tesis/grupos/grupos.php
+++ tesis/grupos/grupos.php
@@ -2,19 +2,17 @@
     <div class="row tarjeta-grupo">
         <div class="col tarjeta">
             <div class="card" style="width: 18rem;">
-                <img src="./img/mat.png" class="card-img-top" alt="imagen del grupo">
+                <img src="./img/mat.png" class="card-img-top img-grupo" alt="imagen del grupo">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
+                    <h5 class="card-title">Nombre del grupo</h5>
                 </div>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item">An item</li>
-                    <li class="list-group-item">A second item</li>
-                    <li class="list-group-item">A third item</li>
+                    <li class="list-group-item">Integrante 1</li>
+                    <li class="list-group-item">Integrante 2</li>
+                    <li class="list-group-item">Integrante 3</li>
                 </ul>
                 <div class="card-body">
-                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Agregar</a>
                     <a href="#" class="card-link">Another link</a>
                 </div>
             </div>
